d4696768e4e729dd4039ce4dfe5c6f36a15c0f52
unknown <gouthamans14@gmial.com>
2022-05-05 10:38:52 +0530

    make it better

--- app.py
+++ app.py
@@ -15,19 +15,27 @@ col1, col2 = st.columns(2)
 with col1:
     with st.form("Prediction_Form"):
         st.header("Enter the deciding Factors:")
-        clone_size=st.text_input('Clone_Size')
+        
+        fruitset=st.text_input('fruitset')
         osmia=st.text_input("Osmia")
-        fruitset=st.text_input("fruitset")
         seeds=st.text_input("seeds")
         fruitmass=st.text_input('fruitmass')
         RainingDays=st.text_input('RainingDays')
+        AverageRainingDays=st.text_input('AverageRainingDays')
+        honeybee = st.text_input('honeybee')
+        andrena = st.text_input('andrena')
+        MaxOfLowerTRange = st.text_input('MaxOfLowerTRange') 
+        AverageOfUpperTRange=st.text_input('AverageOfUpperTRange') 
+        
         submit_val= st.form_submit_button('Predict_yield')
    
 if submit_val:
     #print(clone_size)
     
     #print(type(seeds))
-    ls=np.array([float(seeds),float(fruitset),float(osmia),float(fruitmass),float(RainingDays)]).reshape(1,-1)
+    ls=np.array([float(seeds),float(fruitset),float(osmia),float(fruitmass),float(honeybee),
+    float(andrena),float(RainingDays),float(MaxOfLowerTRange),float(AverageRainingDays),float(AverageOfUpperTRange)]
+    ).reshape(1,-1)
     print(ls)
     value = predict_yield(attributes=ls)
     
--- xgbmodel.py
+++ xgbmodel.py
@@ -20,15 +20,15 @@ import joblib
 
 from xgboost import XGBRFRegressor
 
-url = "C:\\Users\\gouthaman\\Desktop\\WildBlueberryPollinationSimulationData.csv"
+url = "F:\\TMLC\\BlueBerry Prediction\\Dataset\WildBlueberryPollinationSimulationData.csv"
 
 df = pd.read_csv(url)
 
 #print(df.head())
 
-features=['seeds', 'fruitset', 'osmia', 'fruitmass', 'RainingDays']
-xgbref =joblib.load("F://TMLC/BlueBerry Prediction//XGBRegressor1.pkl")
-print(xgbref.get_booster().feature_names)
+features=['seeds','fruitset','osmia','fruitmass','honeybee','andrena','RainingDays','MaxOfLowerTRange','AverageRainingDays','AverageOfUpperTRange']
+xgbref =joblib.load("F://TMLC/BlueBerry Prediction//Lregressor.pkl")
+#print(xgbref.get_booster().feature_names)
 
 def predict_yield(attributes : np.ndarray ):
     pred=xgbref.predict(attributes)
